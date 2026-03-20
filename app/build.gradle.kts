16c06b4165a102c4d0c3d3aff41b7ea99eb2252f
smorelli <saverio.morelli@outlook.it>
2026-03-20 15:58:42 +0100

    2.0
    - minor fixes with languages

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         minSdk = 21
         targetSdk = 35
         versionCode = 70
-        versionName = "2.0"
+        versionName = "2.0-beta"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
